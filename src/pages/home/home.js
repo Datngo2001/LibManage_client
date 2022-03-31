1d8e7d1892982ca0d60c2662ce461051d768325c
DatNgo <ngominhdat110115@gmail.com>
2022-03-31 11:12:38 +0700

    first commit

---
+++ src/pages/home/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './home.scss';
+
+export default function Home() {
+    return (
+        <React.Fragment>
+            <h1>This is home</h1>
+        </React.Fragment>
+    )
+}
